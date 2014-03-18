2221f3fc849e1486d8b6bdd2f9bd0fd20fe7bbdb
Remi Storms <remi.storms@crowdworld.com>
2014-03-18 11:10:23 -0600

    Created demo scene, first test

---
+++ Assets/_Scripts/drinkScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class drinkScript : MonoBehaviour {
+
+	public string drinkName;
+	public float weight = 1.0f;
+	Rigidbody2D drinkRigidBody;
+	public float drinkPrice = 0.0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		this.gameObject.AddComponent<Rigidbody2D>();
+		drinkRigidBody = this.gameObject.GetComponent<Rigidbody2D>();
+		drinkRigidBody.mass = weight;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/_Scripts/gameControl.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameControl : MonoBehaviour {
+
+	public GameObject beer;
+	public GameObject margarita;
+	public GameObject bloodyMary;
+	public GameObject drinkSpawner;
+	public string typeOfDrink;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			SpawnDrink(typeOfDrink);
+		}
+	}
+
+	// function to spawn drinks
+	public void SpawnDrink(string drinkType)
+	{
+		switch (drinkType) 
+		
+		{
+		case "beer":
+				Instantiate(beer, drinkSpawner.transform.position, drinkSpawner.transform.rotation);
+			break;
+
+		case "margarita":
+			Instantiate(margarita, drinkSpawner.transform.position, drinkSpawner.transform.rotation);
+			break;
+
+		case "bloodyMary":
+				Instantiate(bloodyMary, drinkSpawner.transform.position, drinkSpawner.transform.rotation);
+			break;
+
+		} //cierre de switch
+	} //close SpawnDrinks
+
+	public static void MyStaticMethod()
+	{
+		Debug.Log("My static method has been called.");
+	}
+}
